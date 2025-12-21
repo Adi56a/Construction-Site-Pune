e978253d2ca7f071f00ed5a72f52860f438bb46b
Aditya <adityajogdand15@gmail.com>
2025-12-21 15:52:42 +0530

    frontend for create site v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import CreateSite from './pages/CreateSite'
+import Site from './pages/Site'
 
 function App() {
   return (
     <Router> {/* Wrap Routes in Router */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/site" element={<Site />} />
+        <Route path="/create-site" element={<CreateSite />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
